333cdb335ea0c1c4bf8484e8dff01b8e6cdff019
jayyu <jayyu2011@hotmail.com>
2025-03-18 23:45:59 -0700

    info random command

--- proj/1-i2c/tests/1-test-arduino.c
+++ proj/1-i2c/tests/1-test-arduino.c
@@ -11,6 +11,19 @@ void notmain(void) {
     dev_barrier();
     printk("I2C initialized\n");
 
+    // Send a INFO command to the ATECC608A
+    uint8_t info_cmd[] = {0x03, 0x30, 0x00, 0x00};
+    i2c_write(ATECC608A_ADDR, info_cmd, sizeof(info_cmd));
+    printk("INFO command sent\n");
+    delay_ms(300);
+    uint8_t info_data[4];
+    i2c_read(ATECC608A_ADDR, info_data, sizeof(info_data));
+    printk("INFO data received: ");
+    for (int i = 0; i < 4; i++) {
+        printk("%x ", info_data[i]);
+    }
+    printk("\n");
+
     // Send a RANDOM command to the ATECC608A
    uint8_t random_cmd[] = {0x03, 0x1B, 0x00, 0x00};
    i2c_write(ATECC608A_ADDR, random_cmd, sizeof(random_cmd));
@@ -23,15 +36,5 @@ void notmain(void) {
        printk("%x ", random_data[i]);
    }
    printk("\n");
-
-
-   // uint8_t random_data[32];
-   uint8_t random_result;
-   // int random_result = atecc608a_random(random_data);
-   // printk("Random command result: %d\n", random_result);
-
-    // i2c_write(ATECC608A_ADDR, &random_cmd, 1);
-    
-    
-    clean_reboot();
+   clean_reboot();
 }   
