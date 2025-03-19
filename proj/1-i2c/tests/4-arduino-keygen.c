6c06126bc601417e3bcce13798f79d8c0005bb18
jayyu <jayyu2011@hotmail.com>
2025-03-19 00:56:27 -0700

    cleanup

--- proj/1-i2c/tests/4-arduino-keygen.c
+++ proj/1-i2c/tests/4-arduino-keygen.c
@@ -2,6 +2,16 @@
 #include "i2c.h"
 #include "atecc608a.h"
 
+// Write a command to the ATECC608A and read the response   
+int i2c_write_cmd(uint8_t *cmd, size_t cmd_size, 
+                  uint8_t *response, size_t response_size,
+                  unsigned delay_ms_time) {
+    i2c_write(ATECC608A_ADDR, cmd, cmd_size);
+    delay_ms(delay_ms_time);
+    i2c_read(ATECC608A_ADDR, response, response_size);
+    return 0;
+}
+
 void notmain(void) {
     uart_init();
     printk("ATECC608A Detection Test for %x\n", ATECC608A_ADDR);
@@ -13,11 +23,12 @@ void notmain(void) {
 
     // Send a INFO command to the ATECC608A
     uint8_t info_cmd[] = {0x03, 0x30, 0x00, 0x00};
-    i2c_write(ATECC608A_ADDR, info_cmd, sizeof(info_cmd));
-    printk("INFO command sent\n");
-    delay_ms(300);
     uint8_t info_data[4];
-    i2c_read(ATECC608A_ADDR, info_data, sizeof(info_data));
+    i2c_write_cmd(info_cmd, sizeof(info_cmd), info_data, sizeof(info_data), 300);
+    printk("INFO command sent\n");
+    // delay_ms(300);
+    // uint8_t info_data[4];
+    // i2c_read(ATECC608A_ADDR, info_data, sizeof(info_data));
     printk("INFO data received: ");
     for (int i = 0; i < 4; i++) {
         printk("%x ", info_data[i]);
@@ -28,30 +39,17 @@ void notmain(void) {
     printk("Sending GENKEY command...\n");
     uint8_t slot = 0; // Use key slot 0
     uint8_t genkey_cmd[] = {0x07, 0x40, slot, 0x00, 0x00, 0x00, 0x00, 0x00};
-    i2c_write(ATECC608A_ADDR, genkey_cmd, sizeof(genkey_cmd));
+
+    uint8_t genkey_data[64];
+    i2c_write_cmd(genkey_cmd, sizeof(genkey_cmd), genkey_data, sizeof(genkey_data), 500);
+
+    // i2c_write(ATECC608A_ADDR, genkey_cmd, sizeof(genkey_cmd));
     printk("GENKEY command sent to slot %d\n", slot);
-    
-    // Wait for processing - ATECC608A needs time to "generate" the key
-    delay_ms(500);
-    
-    // Read the public key (64 bytes for P256 curve - X and Y coordinates)
-    uint8_t pubkey[64];
-    i2c_read(ATECC608A_ADDR, pubkey, sizeof(pubkey));
-    
-    // Display the public key
-    printk("Public key received:\n");
-    printk("X coordinate: ");
-    for (int i = 0; i < 32; i++) {
-        printk("%x", pubkey[i]);
-        if ((i + 1) % 8 == 0) printk(" ");
-    }
-    printk("\n");
-    
+
+    // Read the public key
     uint8_t pubkey_cmd_x[] = {0x03, 0x23, 0x00, 0x00};
-    i2c_write(ATECC608A_ADDR, pubkey_cmd_x, sizeof(pubkey_cmd_x));
-    delay_ms(500);
     uint8_t pubkey_x[32];
-    i2c_read(ATECC608A_ADDR, pubkey_x, sizeof(pubkey_x));
+    i2c_write_cmd(pubkey_cmd_x, sizeof(pubkey_cmd_x), pubkey_x, sizeof(pubkey_x), 500);
     printk("X coordinate: ");
     for (int i = 0; i < 32; i++) {
         printk("%x", pubkey_x[i]);
@@ -59,10 +57,8 @@ void notmain(void) {
     printk("\n");
 
     uint8_t pubkey_cmd_y[] = {0x03, 0x23, 0x00, 0x01}; // In our Arduino code, we read the Y coordinate from the same address as the X coordinate
-    i2c_write(ATECC608A_ADDR, pubkey_cmd_y, sizeof(pubkey_cmd_y));
-    delay_ms(500);
     uint8_t pubkey_y[32];
-    i2c_read(ATECC608A_ADDR, pubkey_y, sizeof(pubkey_y));
+    i2c_write_cmd(pubkey_cmd_y, sizeof(pubkey_cmd_y), pubkey_y, sizeof(pubkey_y), 500);
     printk("Y coordinate: ");
     for (int i = 0; i < 32; i++) {
         printk("%x", pubkey_y[i]);
