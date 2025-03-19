6e1a6d1d02af6e811064ea201b65a3fa607740bb
jayyu <jayyu2011@hotmail.com>
2025-03-18 23:31:25 -0700

    successfully read 32 bit random from arduino

--- proj/1-i2c/tests/1-test-arduino.c
+++ proj/1-i2c/tests/1-test-arduino.c
@@ -7,11 +7,22 @@ void notmain(void) {
     printk("ATECC608A Detection Test for %x\n", ATECC608A_ADDR);
     
     i2c_init();
+    PUT32(I2C_BASE + 0x1C, 0xFFFF);  // Maximum timeout value
+    dev_barrier();
     printk("I2C initialized\n");
 
     // Send a RANDOM command to the ATECC608A
-   uint8_t random_cmd[] = {0x03, 0x00, 0x1B, 0x00, 0x00};
+   uint8_t random_cmd[] = {0x03, 0x1B, 0x00, 0x00};
    i2c_write(ATECC608A_ADDR, random_cmd, sizeof(random_cmd));
+   printk("Random command sent\n");
+   delay_ms(300);
+   uint8_t random_data[32];
+   i2c_read(ATECC608A_ADDR, random_data, sizeof(random_data));
+   printk("Random data received: ");
+   for (int i = 0; i < 32; i++) {
+       printk("%x ", random_data[i]);
+   }
+   printk("\n");
 
 
    // uint8_t random_data[32];
